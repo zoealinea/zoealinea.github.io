c7be896b64ad533a346081bf4ad572ebdbe1bd31
zoealinea <zoealinea@gmail.com>
2024-03-04 01:04:28 -0700

    editing experience section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,11 +1,43 @@
 // Experience.js
-import React from 'react';
 
+import React, { useState } from 'react';
+import '../styles//Experience.css';
 
 const Experience = () => {
+  const [selectedJob, setSelectedJob] = useState(0);
+
+  const experienceItems = [
+    {
+      title: "Early Warning Services - Zelle",
+      duration: "Month Year - Month Year",
+      description: "Description of job experience 1.",
+    },
+    {
+      title: "Openprise",
+      duration: "Month Year - Month Year",
+      description: "Description of job experience 2.",
+    },
+    // Add more job experiences as needed
+  ];
+
   return (
-    <div className="Experience">
-      Experience Section
+    <div className="experience-container">
+      <div className="tabs">
+        {experienceItems.map((job, index) => (
+          <button
+            key={index}
+            className={`tab ${selectedJob === index ? 'active' : ''}`}
+            onClick={() => setSelectedJob(index)}
+          >
+            {job.title}
+          </button>
+        ))}
+      </div>
+      <div className="job-details">
+        <h2>{experienceItems[selectedJob].title}</h2>
+        <p>{experienceItems[selectedJob].duration}</p>
+        <p>{experienceItems[selectedJob].description}</p>
+      </div>
     </div>
   );
 };
--- src/components/HomeHero.js
+++ src/components/HomeHero.js
@@ -1,27 +1,25 @@
 import '../styles/HomeHero.css';
-
 import React from 'react';
 import { ReactTyped } from 'react-typed';
 
-
 const HomeHero = () => {
   return (
-    <div className="HomeHero large-cursor"> {/* Apply the large-cursor class */}
-      <ReactTyped
-        strings={[
-          "<strong><span class='large-text'>hi i'm <span class='me'>zoe</span></span></strong>",
-          "<strong><span class='large-text'>welcome to</span></strong>",
-          "<strong><span class='large-text'>my <span class='me'>React.js</span> portfolio</span></strong>"
-        ]}
-        typeSpeed={80}
-        backSpeed={40}
-        loop
-        cursorChar={null} // Hide the original cursor provided by ReactTyped
-      />
+    <div className='parent'>
+      <div className="large-cursor"> {/* Apply the large-cursor class */}
+        <ReactTyped
+          strings={[
+            "<strong><span class='large-text'>hi i'm <span class='me'>zoe</span></span></strong>",
+            "<strong><span class='large-text'>welcome to</span></strong>",
+            "<strong><span class='large-text'>my <span class='me'>React.js</span> portfolio</span></strong>"
+          ]}
+          typeSpeed={95}
+          backSpeed={40}
+          loop
+          cursorChar={null} // Hide the original cursor provided by ReactTyped
+        />
+      </div>
     </div>
   );
 };
 
 export default HomeHero;
-
-
