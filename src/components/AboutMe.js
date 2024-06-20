d5e29896082a491b56391f28f3f6d2de1cfcedfc
zoealinea <zoealinea@gmail.com>
2024-06-20 15:36:08 -0700

    update description

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,7 +5,7 @@ import React from 'react';
 const AboutMe = () => {
   return (
     <div className="AboutMe">
-      About Me Section
+      I am a hardworking new graduate from Computer Science Software Engineering at ASU, where I graduated Magna Cum Laude and earned a certification in Informatics. I am eager to further my experience, knowledge, and overall education by pursuing a more challenging environment in the tech industry. I hope to collaborate with others and gain knowledge from their perspectives while also making a positive impact on our ever-changing and developing society.
     </div>
   );
 };
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -9,14 +9,19 @@ const Experience = () => {
   const experienceItems = [
     {
       title: "Early Warning Services - Zelle",
-      duration: "Month Year - Month Year",
-      description: "Description of job experience 1.",
+      duration: "January 2024 - April 2024",
+      description: "Built CI/CD pipelines to help streamline and accelerate the Dev and QA Team’s software development lifecycle. \nThe tech stack included Java Services built with Spring Boot. CD using Harness, Ansible, HashiCorp Vault. \nEdited and validated Ansible Playbooks in GitLab as needed. /nUsed agile methodologies, completed stories assigned in Jira, and wrote thorough documentation. \nConducted a final culminating presentation on project in front of 50+ colleagues and managers.",
     },
     {
       title: "Openprise",
-      duration: "Month Year - Month Year",
-      description: "Description of job experience 2.",
-    },
+      duration: "June 2022 - May 2023",
+      description: "Developed a metrics dashboard for Engineering and Sales teams: \nTo track product performance and customer trends to display in corporate office space. \nDisplay data usage metrics for sales optimization and bug identification. \nBuilt the backend server with the Micronaut framework. \nMade to support seamless data transmission via RESTFUL APIs to interact with MySQL database and Elasticsearch search engine. \nApplied data engineering techniques, focusing on ETL processes. \nDynamic Frontend built with Javascript, HTML, and CSS. \nBuilt utilizing Vue.js, Axios, and Pinia state management. \nEmployed Apache Echarts for real-time, engaging, data visualization. \nNginx reverse proxy, JWT authentication, and OAuth for data security. \nContainerization using Docker and also assisted with deployment.",
+    }
+    // {
+    //   title: "",
+    //   duration: "",
+    //   description: ""
+    // }
     // Add more job experiences as needed
   ];
 
