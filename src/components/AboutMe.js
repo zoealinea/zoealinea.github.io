db01bb7857f1417db357a07ce195b7455797b548
zoealinea <96972217+zoealinea@users.noreply.github.com>
2024-06-21 14:19:35 -0700

    update projects and about me
    also added a lot of styling

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,16 @@
 // AboutMe.js
-import React from 'react';
 
+import React from 'react';
+import '../styles/AboutMe.css';
+import me from '../images/me.jpeg';
 
 const AboutMe = () => {
   return (
     <div className="AboutMe">
-      I am a hardworking new graduate from Computer Science Software Engineering at ASU, where I graduated Magna Cum Laude and earned a certification in Informatics. I am eager to further my experience, knowledge, and overall education by pursuing a more challenging environment in the tech industry. I hope to collaborate with others and gain knowledge from their perspectives while also making a positive impact on our ever-changing and developing society.
+      <img src={me} alt="Me" />
+      <p>
+        I am a hardworking new graduate from Computer Science Software Engineering at ASU, where I graduated Magna Cum Laude and earned a certification in Informatics. I am eager to further my experience, knowledge, and overall education by pursuing a more challenging environment in the tech industry. I hope to collaborate with others and gain knowledge from their perspectives while also making a positive impact on our ever-changing and developing society.
+      </p>
     </div>
   );
 };
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,38 @@
 // Projects.js
 import React from 'react';
-
+import '../styles/Projects.css';
+import { FaGithub } from 'react-icons/fa';
 
 const Projects = () => {
+  const projectData = [
+    {
+      title: 'GDMS - Asset Tracking Tool',
+      description: 'A web-based asset tracking tool that tracks local assets in a given space.',
+      githubLink: 'https://github.com/kevbao/Capstone-AssetTrackingTool'
+    },
+    {
+      title: 'zoealinea.github.io',
+      description: 'My personal website portfolio project.',
+      githubLink: 'https://github.com/zoealinea/zoealinea.github.io'
+    },
+    {
+      title: 'Project Three',
+      description: 'Coming Soon!',
+      githubLink: ''
+    }
+  ];
+
   return (
     <div className="Projects">
-      Projects Section
+      {projectData.map((project, index) => (
+        <div className="ProjectTile" key={index}>
+          <h3>{project.title}</h3>
+          <p>{project.description}</p>
+          <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+            <FaGithub size={32} />
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
