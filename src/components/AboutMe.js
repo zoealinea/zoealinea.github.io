5dcd0e78737f19d9369acc23de444ce612cfab18
zoealinea <96972217+zoealinea@users.noreply.github.com>
2024-06-23 22:27:53 -0700

    style changes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,9 +6,15 @@ const AboutMe = () => {
   return (
     <div className="AboutMe">
       <img src={me} alt="Me" />
-      <p>
-        I am a hardworking new graduate from <span class='highlight'>Computer Science Software Engineering</span> at ASU, where I graduated <span class='highlight'>Magna Cum Laude</span> and earned a certification in <span class='highlight'>Informatics</span>. I am eager to further my experience, knowledge, and overall education by pursuing a more challenging environment in the tech industry. I hope to collaborate with others and gain knowledge from their perspectives while also making a positive impact on our ever-changing and developing society.
-      </p>
+      <div>
+        <p>
+          I am a hardworking new graduate from <span class='highlight'>Computer Science Software Engineering</span> at ASU, where I graduated <span class='highlight'>Magna Cum Laude</span> and earned a certification in <span class='highlight'>Informatics</span>. I am eager to further my experience, knowledge, and overall education by pursuing a more challenging environment in the tech industry. I hope to collaborate with others and gain knowledge from their perspectives while also making a positive impact on our ever-changing and developing society.
+        </p>
+        <p>Here are a few of the techonologies I have been working with: </p>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
     </div>
   );
 };
